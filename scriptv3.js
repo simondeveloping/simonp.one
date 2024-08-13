e6457f4dda7e8689b09019be2ba96b3384f1f17f
Pham <yv521464@win.rz.rwth-aachen.de>
2024-08-13 14:05:19 +0200

    make responsive design more beautiful

--- scriptv3.js
+++ scriptv3.js
@@ -12,42 +12,108 @@ function addToList() {
         document.getElementById("suggestioninput").value = '';
     }
 }
-document.getElementById("suggestionsubmit").addEventListener("click", function (event) {
-    event.preventDefault();
-    const email = "simon.pham@web.de";
-    const subject = "Feedback Website";
-    const list = document.getElementsByTagName('li');
-    let mailtext = '';
-    for (let i = 0; i < list.length; i++) {
-        mailtext += list[i].textContent + '\n';
-        console.log(mailtext);
-    }
-    const body = encodeURIComponent(mailtext);
-    suggestionsubmit.href = `mailto:${email}?subject=${(subject)}&body=${body}`;
-    window.location.href = suggestionsubmit.href;
-})
-document.getElementById("login-form").addEventListener("submit", async function (event) {
-    event.preventDefault();
-    let password = document.getElementById('passwordforprivat').value;
-    let encrypt = await decrypt(password);
-    if (encrypt === "9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08") {
-        document.getElementById("privatebox1").href = "/pinguin/pinguin";
-        document.getElementById("privatebox1").style.e;
 
-        document.getElementById("privatebox2").href = "/pinguin/pinguin";
-        document.getElementById("privatebox3").href = "/pinguin/pinguin";
-        document.getElementById("privateboxout1").style.backgroundColor = "white";
-        document.getElementById("privateboxout2").style.backgroundColor = "white";
-        document.getElementById("privateboxout3").style.backgroundColor = "white";
-    } else {
-        alert("Wrong password");
-    }
+document.getElementById("arrowleft").addEventListener("click",function(event){
+    event.preventDefault();
+    scrollLeft1();
 })
-async function decrypt(text) {
-    const encoder = new TextEncoder();
-    const data = encoder.encode(text);
-    const hashBuffer = await crypto.subtle.digest('SHA-256', data);
-    const hashArray = Array.from(new Uint8Array(hashBuffer));
-    const hashHex = hashArray.map(byte => byte.toString(16).padStart(2, '0')).join('');
-    return hashHex;
+document.getElementById("arrowright").addEventListener("click",function(event){
+    event.preventDefault();
+    scrollRight1();
+})
+function scrollLeft1(){
+    document.querySelector(".projectbox").scrollBy({
+        left:-200,
+        behavior:"smooth"
+    })
 }
+function scrollRight1(){
+    document.querySelector(".projectbox").scrollBy({
+        left:200,
+        behavior:"smooth"
+    })
+}
+document.getElementById("privatearrowleft").addEventListener("click",function(event){
+    event.preventDefault();
+    scrollLeft();
+})
+document.getElementById("privatearrowright").addEventListener("click",function(event){
+    event.preventDefault();
+    scrollRight();
+})
+function scrollLeft(){
+    document.querySelector(".privatestuffbox").scrollBy({
+        left:-200,
+        behavior:"smooth"
+    })
+}
+function scrollRight(){
+    document.querySelector(".privatestuffbox").scrollBy({
+        left:200,
+        behavior:"smooth"
+    })
+}
+document.getElementById("arrowfbleft").addEventListener("click",function(event){
+    event.preventDefault();
+    scrollLeft2();
+})
+document.getElementById("arrowfbright").addEventListener("click",function(event){
+    event.preventDefault();
+    scrollRight2();
+})
+function scrollLeft2(){
+    document.querySelector(".feedbackoverlay").scrollBy({
+        left:-200,
+        behavior:"smooth"
+    })
+}
+function scrollRight2(){
+    document.querySelector(".feedbackoverlay").scrollBy({
+        left:200,
+        behavior:"smooth"
+    })
+}
+
+ScrollReveal().reveal('.sectiontitle', {
+    origin: 'bottom',
+    distance: '50px',
+    duration: 1000,
+    easing: 'ease-in-out',
+    delay: 200,
+    reset:true
+});
+
+ScrollReveal().reveal('.projectbox', {
+    origin: 'bottom',
+    distance: '100px',
+    duration: 1000,
+    easing: 'ease-in-out',
+    delay: 400,
+    reset:true
+});
+
+ScrollReveal().reveal('.privatestuffbox', {
+    origin: 'bottom',
+    distance: '100px',
+    duration: 1000,
+    easing: 'ease-in-out',
+    delay: 600,
+    reset:true
+});
+
+ScrollReveal().reveal('.aboutthispagelayer', {
+    origin: 'bottom',
+    distance: '100px',
+    duration: 1000,
+    easing: 'ease-in-out',
+    delay: 600,
+    reset:true
+});
+ScrollReveal().reveal('.feedbackoverlay', {
+    origin: 'bottom',
+    distance: '100px',
+    duration: 1000,
+    easing: 'ease-in-out',
+    delay: 600,
+    reset:true
+});
