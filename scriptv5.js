6290c2da091dfc223551e9929fb675c05b57f3c1
Pham <yv521464@win.rz.rwth-aachen.de>
2024-08-16 16:19:50 +0200

    test

--- scriptv5.js
+++ scriptv5.js
@@ -12,17 +12,116 @@ function addToList() {
         document.getElementById("suggestioninput").value = '';
     }
 }
-document.getElementById("suggestionsubmit").addEventListener("click", function (event) {
+
+document.getElementById("arrowleft").addEventListener("click",function(event){
     event.preventDefault();
-    const email = "simon.pham@web.de";
-    const subject = "Feedback Website";
-    const list = document.getElementsByTagName('li');
-    let mailtext = '';
-    for (let i = 0; i < list.length; i++) {
-        mailtext += list[i].textContent + '\n';
-        console.log(mailtext);
-    }
-    const body = encodeURIComponent(mailtext);
-    suggestionsubmit.href = `mailto:${email}?subject=${(subject)}&body=${body}`;
-    window.location.href = suggestionsubmit.href;
+    scrollLeft1();
 })
+document.getElementById("arrowright").addEventListener("click",function(event){
+    event.preventDefault();
+    scrollRight1();
+})
+function scrollLeft1(){
+    document.querySelector(".projectbox").scrollBy({
+        left:-200,
+        behavior:"smooth"
+    })
+}
+function scrollRight1(){
+    document.querySelector(".projectbox").scrollBy({
+        left:200,
+        behavior:"smooth"
+    })
+}
+document.getElementById("privatearrowleft").addEventListener("click",function(event){
+    event.preventDefault();
+    scrollLeft();
+})
+document.getElementById("privatearrowright").addEventListener("click",function(event){
+    event.preventDefault();
+    scrollRight();
+})
+function scrollLeft(){
+    document.querySelector(".privatestuffbox").scrollBy({
+        left:-200,
+        behavior:"smooth"
+    })
+}
+function scrollRight(){
+    document.querySelector(".privatestuffbox").scrollBy({
+        left:200,
+        behavior:"smooth"
+    })
+}
+document.getElementById("arrowfbleft").addEventListener("click",function(event){
+    event.preventDefault();
+    scrollLeft2();
+})
+document.getElementById("arrowfbright").addEventListener("click",function(event){
+    event.preventDefault();
+    scrollRight2();
+})
+function scrollLeft2(){
+    document.querySelector(".feedbackoverlay").scrollBy({
+        left:-200,
+        behavior:"smooth"
+    })
+}
+function scrollRight2(){
+    document.querySelector(".feedbackoverlay").scrollBy({
+        left:200,
+        behavior:"smooth"
+    })
+}
+
+ScrollReveal().reveal('.sectiontitle', {
+    origin: 'bottom',
+    distance: '50px',
+    duration: 1000,
+    easing: 'ease-in-out',
+    delay: 200,
+    reset:true
+});
+
+ScrollReveal().reveal('.projectbox', {
+    origin: 'bottom',
+    distance: '100px',
+    duration: 1000,
+    easing: 'ease-in-out',
+    delay: 400,
+    reset:true
+});
+
+ScrollReveal().reveal('.privatestuffbox', {
+    origin: 'bottom',
+    distance: '100px',
+    duration: 1000,
+    easing: 'ease-in-out',
+    delay: 600,
+    reset:true
+});
+
+ScrollReveal().reveal('.aboutthispagelayer', {
+    origin: 'bottom',
+    distance: '100px',
+    duration:1000,
+    easing: 'ease-in-out',
+    delay: 600,
+    reset:true
+});
+ScrollReveal().reveal('.feedbackoverlay', {
+    origin: 'bottom',
+    distance: '100px',
+    duration: 1000,
+    easing: 'ease-in-out',
+    delay: 600,
+    reset:true
+});
+ScrollReveal().reveal('.aboutmecontainer', {
+    origin: 'bottom',
+    distance: '100px',
+    duration: 1000,
+    easing: 'ease-in-out',
+    delay: 600,
+    reset:true
+});
