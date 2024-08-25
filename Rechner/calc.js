7a44c7cddb3523f847ddeeb7f16167373406b9fe
Pham <yv521464@win.rz.rwth-aachen.de>
2024-08-25 17:50:20 +0200

    make more functions in calc

--- Rechner/calc.js
+++ Rechner/calc.js
@@ -1,7 +1,10 @@
-let sum;
-let value1;
-let value2;
+let sum = null;
+let value1 = null;
+let value2 = null;
+let operator = null;    
+
 let input = document.getElementById("value");
+input.value = "0";
 document.getElementById("plus").addEventListener("click", function(event){
     event.preventDefault();
     
@@ -9,14 +12,14 @@ document.getElementById("plus").addEventListener("click", function(event){
 document.querySelectorAll(".gray").forEach(button => button.addEventListener("click",function(event){
     event.preventDefault();
     let number = this.textContent.trim();
-    if(number === "." && input.value.includes(".")){
+    if(number === "," && input.value.includes(",")){
         return;
     }
-    if(input.value === "0" || input.value === ""){
-        if(number ==="." && input.value === ""){    
-            input.value = "0.";
-        }else if(number === "." && input.value !== ""){
-            input.value += ".";
+    if(input.value === "0"){
+        if(number ==="," && input.value === "0"){    
+            input.value = "0,";
+        }else if(number === ","){
+            input.value += ",";
         }
         else{
             input.value = number;
@@ -26,11 +29,14 @@ document.querySelectorAll(".gray").forEach(button => button.addEventListener("cl
     }
 }))
 document.getElementById("ac").addEventListener("click",function(event){
-    input.value = "";
+    input.value = "0";
+    value1 = null;
+    value2 = null;
+    sum = null;
 })
-document.getElementById("operator").addEventListener("click",function(event){
+document.getElementById("sign").addEventListener("click",function(event){
     event.preventDefault();
-    if(input.value === ""){
+    if(input.value === "0"){
         return;
     }
     if(input.value < 0){
@@ -41,5 +47,43 @@ document.getElementById("operator").addEventListener("click",function(event){
 })
 document.getElementById("equal").addEventListener("click",function(event){
     event.preventDefault();
-    input.value = sum;
+    value2 = parseFloat(input.value.replace(",","."));
+    if(operator ==="+"){
+       sum = value1 + value2;
+    }else if(operator === "-"){
+        sum = value1 - value2;
+    }else if(operator === "x"){
+        sum = value1 * value2;
+    }else if(operator === "/"){
+        sum = value1 / value2;
+    }
+    input.value = sum.toString();
+})
+document.getElementById("plus").addEventListener("click",function(event){
+    event.preventDefault();
+    operator = "+";
+    value1 = parseFloat(input.value.replace(",","."));
+    input.value = "";
+    document.getElementById("plus").style.backgroundColor = "rgb(245, 204, 91)";
+})
+document.getElementById("minus").addEventListener("click",function(event){
+    event.preventDefault();
+    operator = "-";
+    value1 = parseFloat(input.value.replace(",","."));
+    input.value = "";
+    document.getElementById("minus").style.backgroundColor = "rgb(245, 204, 91)";
+})
+document.getElementById("multiply").addEventListener("click",function(event){
+    event.preventDefault();
+    operator = "x";
+    value1 = parseFloat(input.value.replace(",","."));
+    input.value = "";
+    document.getElementById("multiply").style.backgroundColor = "rgb(245, 204, 91)";
+})
+document.getElementById("divide").addEventListener("click",function(event){
+    event.preventDefault();
+    operator = "/";
+    value1 = parseFloat(input.value.replace(",","."));
+    input.value = "";
+    document.getElementById("divide").style.backgroundColor = "rgb(245, 204, 91)";
 })
