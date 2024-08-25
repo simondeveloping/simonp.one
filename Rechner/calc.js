f0f4b02ecb7ed7ad7a9235a1f2dc676547666d50
Pham <yv521464@win.rz.rwth-aachen.de>
2024-08-25 15:12:58 +0200

    added design calc

--- Rechner/calc.js
+++ Rechner/calc.js
@@ -9,14 +9,17 @@ document.getElementById("plus").addEventListener("click", function(event){
 document.querySelectorAll(".gray").forEach(button => button.addEventListener("click",function(event){
     event.preventDefault();
     let number = this.textContent.trim();
+    if(number === "." && input.value.includes(".")){
+        return;
+    }
     if(input.value === "0" || input.value === ""){
-        if(number === "." && input.value.includes(".")){
-            return;
+        if(number ==="." && input.value === ""){    
+            input.value = "0.";
+        }else if(number === "." && input.value !== ""){
+            input.value += ".";
         }
-        if(number ==="."){
-            input.value += "0.";
-        }else{
-        input.value = number;
+        else{
+            input.value = number;
         }
     }else{
     input.value +=number;
@@ -24,4 +27,19 @@ document.querySelectorAll(".gray").forEach(button => button.addEventListener("cl
 }))
 document.getElementById("ac").addEventListener("click",function(event){
     input.value = "";
+})
+document.getElementById("operator").addEventListener("click",function(event){
+    event.preventDefault();
+    if(input.value === ""){
+        return;
+    }
+    if(input.value < 0){
+        input.value = input.value.replace("-","");
+    }else{
+        input.value = "-" + input.value;
+    }
+})
+document.getElementById("equal").addEventListener("click",function(event){
+    event.preventDefault();
+    input.value = sum;
 })
