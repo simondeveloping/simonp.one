d59496b75dd037806e4a51b8a0f7c4fad29af7ad
Pham <yv521464@win.rz.rwth-aachen.de>
2024-08-26 14:37:14 +0200

    fix bug in calculator

--- Calculator/calcv5.js
+++ Calculator/calcv5.js
@@ -10,7 +10,7 @@ let sum = null;
 let value1 = null;
 let value2 = null;
 let operator = null;
-
+let reset = false;
 let input = document.getElementById("value");
 input.value = "0";
 function updateClear(){
@@ -26,14 +26,17 @@ document.getElementById("plus").addEventListener("click", function (event) {
 document.querySelectorAll(".gray").forEach((button) =>
   button.addEventListener("click", function (event) {
     event.preventDefault();
-    input.value = "";
     let number = this.textContent.trim();
+    if(reset){
+      input.value = "";
+      reset = false;
+    }
     resetColors();
     if (number === "," && input.value.includes(",")) {
       return;
     }
     if (input.value === "0") {
-      if (number === "," && input.value === "0") {
+      if (number === ",") {
         input.value = "0,";
       } else if (number === ",") {
         input.value += ",";
@@ -80,6 +83,7 @@ document.getElementById("equal").addEventListener("click", function (event) {
   input.value = sum.toString().replace(".", ",");
   value1 = sum;
   operator = null;
+  reset = true;
   resetColors();
 });
 document.getElementById("plus").addEventListener("click", function (event) {
@@ -105,21 +109,22 @@ function resetColors() {
   document.getElementById("divide").style.backgroundColor = "orange";
 }
 function addOperation(selectedOperator, button) {
-  if (operator !== null) {
+  if (operator !== null && !reset) {
     value2 = parseFloat(input.value.replace(",", "."));
     if (operator === "+") {
-      value1 += parseFloat(value2);
+      value1 += value2;
     } else if (operator === "-") {
-      value1 -= parseFloat(value2);
+      value1 -= value2;
     } else if (operator === "x") {
-      value1 *= parseFloat(value2);
+      value1 *= value2;
     } else if (operator === "/") {
-      value1 /= parseFloat(value2);
+      value1 /= value2;
     }
   } else {
     value1 = parseFloat(input.value.replace(",", "."));
   }
   operator = selectedOperator;
+  reset = true;
   resetColors();
   button.style.backgroundColor = "rgb(245, 204, 91)";
 }
