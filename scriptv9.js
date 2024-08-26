c1deb9857d6c9529cbda051bfdeacbf808ac0db9
Pham <yv521464@win.rz.rwth-aachen.de>
2024-08-26 10:53:37 +0200

    change desgin

--- scriptv9.js
+++ scriptv9.js
@@ -1,3 +1,4 @@
+/*
 document.getElementById("suggestionform").addEventListener("submit", function (event) {
     event.preventDefault();
     addToList();
@@ -12,6 +13,7 @@ function addToList() {
         document.getElementById("suggestioninput").value = '';
     }
 }
+*/
 
 document.getElementById("arrowleft").addEventListener("click",function(event){
     event.preventDefault();
@@ -22,13 +24,13 @@ document.getElementById("arrowright").addEventListener("click",function(event){
     scrollRight1();
 })
 function scrollLeft1(){
-    document.querySelector(".projectbox").scrollBy({
+    document.querySelector(".miniprojectbox").scrollBy({
         left:-200,
         behavior:"smooth"
     })
 }
 function scrollRight1(){
-    document.querySelector(".projectbox").scrollBy({
+    document.querySelector(".miniprojectbox").scrollBy({
         left:200,
         behavior:"smooth"
     })
@@ -55,22 +57,98 @@ function scrollRight(){
 }
 document.getElementById("arrowfbleft").addEventListener("click",function(event){
     event.preventDefault();
-    scrollLeft();
+    scrollLeft2();
 })
 document.getElementById("arrowfbright").addEventListener("click",function(event){
     event.preventDefault();
-    scrollRight();
+    scrollRight2();
 })
-function scrollLeft(){
+function scrollLeft2(){
     document.querySelector(".feedbackoverlay").scrollBy({
         left:-200,
         behavior:"smooth"
     })
 }
-function scrollRight(){
+function scrollRight2(){
     document.querySelector(".feedbackoverlay").scrollBy({
         left:200,
         behavior:"smooth"
     })
 }
 
+ScrollReveal().reveal('.sectiontitle', {
+    origin: 'bottom',
+    distance: '50px',
+    duration: 1000,
+    easing: 'ease-in-out',
+    delay: 200,
+    reset:true
+});
+
+ScrollReveal().reveal('.miniprojectbox', {
+    origin: 'bottom',
+    distance: '100px',
+    duration: 1000,
+    easing: 'ease-in-out',
+    delay: 400,
+    reset:true
+});
+
+ScrollReveal().reveal('.privatestuffbox', {
+    origin: 'bottom',
+    distance: '100px',
+    duration: 1000,
+    easing: 'ease-in-out',
+    delay: 600,
+    reset:true
+});
+
+ScrollReveal().reveal('.aboutthispagelayer', {
+    origin: 'bottom',
+    distance: '100px',
+    duration:1000,
+    easing: 'ease-in-out',
+    delay: 600,
+    reset:true
+});
+ScrollReveal().reveal('.feedbackoverlay', {
+    origin: 'bottom',
+    distance: '100px',
+    duration: 1000,
+    easing: 'ease-in-out',
+    delay: 600,
+    reset:true
+});
+ScrollReveal().reveal('.aboutmecontainer', {
+    origin: 'bottom',
+    distance: '100px',
+    duration: 1000,
+    easing: 'ease-in-out',
+    delay: 600,
+    reset:true
+});
+
+let numberDays;
+let maxNumberDays;
+const year = new Date().getFullYear();
+const firstDayInYear = new Date(new Date().getFullYear(),0,1);
+const currentDay = new Date();
+
+function percent(){
+    if((year % 4 ===0 && year % 100 !== 0) || year % 400 === 0){
+        maxNumberDays = 366;
+    }else{
+        maxNumberDays = 365;
+    }
+    numberDays = Math.round(Math.abs(firstDayInYear - currentDay) / (1000 * 60 * 60 *24))
+}
+function days(){
+    let filled =document.getElementById("timefilled");
+    let percent= (numberDays / maxNumberDays) * 100;
+    filled.style.width = percent + "%";
+    document.getElementById("daysOver").innerHTML = numberDays + " / "+ maxNumberDays +" Days";
+    document.getElementById("daysPercent").innerHTML = percent.toFixed(1) + "%";
+    document.getElementById("currentYear").innerHTML += year;
+}
+percent();
+days();
