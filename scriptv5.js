b162c88f24ba1e1222eff7b394513c302c5bec15
Pham <yv521464@win.rz.rwth-aachen.de>
2024-08-28 15:13:48 +0200

    make it for mobile

--- scriptv5.js
+++ scriptv5.js
@@ -1,3 +1,4 @@
+/*
 document.getElementById("suggestionform").addEventListener("submit", function (event) {
     event.preventDefault();
     addToList();
@@ -12,6 +13,7 @@ function addToList() {
         document.getElementById("suggestioninput").value = '';
     }
 }
+*/
 
 document.getElementById("arrowleft").addEventListener("click",function(event){
     event.preventDefault();
@@ -22,13 +24,13 @@ document.getElementById("arrowright").addEventListener("click",function(event){
     scrollRight1();
 })
 function scrollLeft1(){
-    document.querySelector(".projectbox").scrollBy({
+    document.querySelector(".miniprojectbox").scrollBy({
         left:-200,
         behavior:"smooth"
     })
 }
 function scrollRight1(){
-    document.querySelector(".projectbox").scrollBy({
+    document.querySelector(".miniprojectbox").scrollBy({
         left:200,
         behavior:"smooth"
     })
@@ -73,23 +75,20 @@ function scrollRight2(){
         behavior:"smooth"
     })
 }
-
 ScrollReveal().reveal('.sectiontitle', {
     origin: 'bottom',
     distance: '50px',
     duration: 1000,
     easing: 'ease-in-out',
     delay: 200,
-    reset:true
 });
 
-ScrollReveal().reveal('.projectbox', {
+ScrollReveal().reveal('.miniprojectbox', {
     origin: 'bottom',
     distance: '100px',
     duration: 1000,
     easing: 'ease-in-out',
     delay: 400,
-    reset:true
 });
 
 ScrollReveal().reveal('.privatestuffbox', {
@@ -98,7 +97,6 @@ ScrollReveal().reveal('.privatestuffbox', {
     duration: 1000,
     easing: 'ease-in-out',
     delay: 600,
-    reset:true
 });
 
 ScrollReveal().reveal('.aboutthispagelayer', {
@@ -107,7 +105,6 @@ ScrollReveal().reveal('.aboutthispagelayer', {
     duration:1000,
     easing: 'ease-in-out',
     delay: 600,
-    reset:true
 });
 ScrollReveal().reveal('.feedbackoverlay', {
     origin: 'bottom',
@@ -115,7 +112,6 @@ ScrollReveal().reveal('.feedbackoverlay', {
     duration: 1000,
     easing: 'ease-in-out',
     delay: 600,
-    reset:true
 });
 ScrollReveal().reveal('.aboutmecontainer', {
     origin: 'bottom',
@@ -123,5 +119,76 @@ ScrollReveal().reveal('.aboutmecontainer', {
     duration: 1000,
     easing: 'ease-in-out',
     delay: 600,
+});
+ScrollReveal().reveal('.projectbox', {
+    origin: 'bottom',
+    distance: '100px',
+    duration: 1000,
+    easing: 'ease-in-out',
+    delay: 600,
+});
+ScrollReveal().reveal('.watercontainer', {
+    origin: 'bottom',
+    distance: '100px',
+    duration: 1000,
+    easing: 'ease-in-out',
+    delay: 600,
+});
+ScrollReveal().reveal('nav', {
+    origin: 'top',
+    distance: '50px',
+    duration: 1000,
+    easing: 'ease-in-out',
+    delay: 1,
     reset:true
+});
+
+let numberDays;
+let maxNumberDays;
+const year = new Date().getFullYear();
+const firstDayInYear = new Date(new Date().getFullYear(),0,1);
+const currentDay = new Date();
+
+function percent(){
+    if((year % 4 ===0 && year % 100 !== 0) || year % 400 === 0){
+        maxNumberDays = 366;
+    }else{
+        maxNumberDays = 365;
+    }
+    numberDays = Math.round(Math.abs(firstDayInYear - currentDay) / (1000 * 60 * 60 *24))
+}
+function days(){
+    let filled =document.getElementById("timefilled");
+    let percent= (numberDays / maxNumberDays) * 100;
+    filled.style.width = percent + "%";
+    document.getElementById("daysOver").innerHTML = numberDays + " / "+ maxNumberDays +" Days";
+    document.getElementById("daysPercent").innerHTML = percent.toFixed(1) + "%";
+    document.getElementById("currentYear").innerHTML += year;
+}
+percent();
+days();
+if(window.matchMedia("(max-width: 768px)").matches){
+    document.querySelector(".projectbox").classList.add("hidden");
+}
+document.getElementById("closeProject").addEventListener("click",function(){
+    if(!document.querySelector(".projectbox").classList.contains("hidden")){
+    document.querySelector(".projectbox").classList.add("hidden");
+    }else{
+        document.querySelector(".projectbox").classList.remove("hidden");
+    }
+});
+document.getElementById("bar").addEventListener("click",function(e){
+    e.preventDefault();
+    let bar = document.getElementById("menuBar");
+    bar.classList.toggle("open");
+    this.classList.add("hidden");
+});
+document.addEventListener("click", function(e) {
+    let bar = document.getElementById("menuBar");
+    let barButton = document.getElementById("bar");
+
+    if (!bar.contains(e.target) && !barButton.contains(e.target)) {
+        bar.classList.remove("open"); 
+        document.getElementById("bar").classList.remove("hidden");
+    }
 });
